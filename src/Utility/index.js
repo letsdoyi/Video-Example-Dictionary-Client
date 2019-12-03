e730cfb5331e1024a1d7f879cc0f90c0cd4fcc82
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2019-12-03 18:00:28 +0900

    Added secondsConverter in Utility

--- src/Utility/index.js
+++ src/Utility/index.js
@@ -8,4 +8,29 @@ function dividedTimeToOne(dividedTimeString, seperator) {
   return one;
 }
 
-export { dividedTimeToOne, oneToTwoDigits };
+function secondsConverter(number, unit) {
+  let d, h, m, s;
+
+  if (isNaN(number)) {
+    throw new TypeError("Value sent to seconds-converter must be a number.");
+  }
+
+  if (unit === "sec" || unit === "seconds") {
+    s = number;
+  } else if (unit === "ms" || unit === "milliseconds" || !unit) {
+    s = Math.floor(number / 1000);
+  } else {
+    throw new TypeError("Unit must be /'sec'/ or /'ms'/");
+  }
+
+  m = Math.floor(s / 60);
+  s = s % 60;
+  h = Math.floor(m / 60);
+  m = m % 60;
+  d = Math.floor(h / 24);
+  h = h % 24;
+
+  return { days: d, hours: h, minutes: m, seconds: s };
+}
+
+export { dividedTimeToOne, oneToTwoDigits, secondsConverter };
