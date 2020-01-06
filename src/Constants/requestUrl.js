da06cd35af61c6a31fcf29d0690c7c5be077e539
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2020-01-06 17:03:42 +0900

    Changed url to api.letsdoyi.com

--- src/Constants/requestUrl.js
+++ src/Constants/requestUrl.js
@@ -1,14 +1,14 @@
 const REQUEST_URL = {
   LOGIN_SUCCESS:
-    'http://localhost:4000/api/auth/google/login/success',
-  LOGOUT: 'http://localhost:4000/api/auth/google/logout',
-  POST_SEARCH_RESULT_FOR_VIDEO: 'http://localhost:4000/api/videos',
+    'https://api.letsdoyi.com/api/auth/google/login/success',
+  LOGOUT: 'https://api.letsdoyi.com/api/auth/google/logout',
+  POST_SEARCH_RESULT_FOR_VIDEO: 'https://api.letsdoyi.com/api/videos',
   GET_VIDEO_SUCCESS:
-  'http://localhost:4000/api/videos/success',
-  POST_SEARCH_RESULT_FOR_DICTIONARY: 'http://localhost:4000/api/dictionary',
-  GET_DICTIONARY_SUCCESS: 'http://localhost:4000/api/dictionary/success',
-  POST_ADDED_WORD: 'http://localhost:4000/api/myWords',
-  DELETE_WORD: 'http://localhost:4000/api/myWords'
+  'https://api.letsdoyi.com/api/videos/success',
+  POST_SEARCH_RESULT_FOR_DICTIONARY: 'https://api.letsdoyi.com/api/dictionary',
+  GET_DICTIONARY_SUCCESS: 'https://api.letsdoyi.com/api/dictionary/success',
+  POST_ADDED_WORD: 'https://api.letsdoyi.com/api/myWords',
+  DELETE_WORD: 'https://api.letsdoyi.com/api/myWords'
 };
 
 export default REQUEST_URL;
