863e24695027c52ecff489ce79cae4ba19ea8daf
DoyiKim <doyi@DoyiKimui-MacBookPro.local>
2020-01-11 23:00:21 +0900

    Deleted comments

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,19 @@
-import React, { useEffect } from 'react';
-import { Route } from 'react-router-dom';
-import Login from '../Components/Login';
-import Header from '../Components/Header';
-import Home from '../Components/Home';
-import Videos from '../Components/Videos';
-import MyWords from '../Components/MyWords';
-import axios from 'axios';
-import { oneToTwoDigits, secondsConverter } from '../Utility';
-import './Common.scss';
-import './App.scss';
-import { SERVER_URL, REQUEST } from '../Constants/requestURL';
+import React, { useEffect } from "react";
+import { Route } from "react-router-dom";
+import Login from "../Components/Login";
+import Header from "../Components/Header";
+import Home from "../Components/Home";
+import Videos from "../Components/Videos";
+import MyWords from "../Components/MyWords";
+import axios from "axios";
+import { oneToTwoDigits, secondsConverter } from "../Utility";
+import "./Common.scss";
+import "./App.scss";
+import { SERVER_URL, REQUEST } from "../Constants/requestURL";
 
 function App(props) {
   console.log(props);
   const {
-    dictionary,
     request,
     selected,
     getUserData,
@@ -23,29 +22,28 @@ function App(props) {
     myWords,
     updateMyWords,
     userInfo,
-    isLoggedIn,
-    addVideoInfoToDictionary,
+    addVideoInfoToDictionary
   } = props;
 
   useEffect(props => {
     const fetchLoginData = async () => {
-      console.log('유저 가져오기');
+      console.log("유저 가져오기");
       const response = await axios.get(SERVER_URL + REQUEST.LOGIN_SUCCESS, {
-        withCredentials: 'include',
+        withCredentials: "include",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Credentials': true,
-        },
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true
+        }
       });
       console.log(response);
       const user = response.data.user;
       if (user) {
         getUserData(user);
-        console.log('유저 결과 확인:', user);
+        console.log("유저 결과 확인:", user);
         const myWordsKeys = Object.keys(user.my_words);
         myWordsKeys.forEach(wordKey => {
-          updateMyWords(user.my_words[wordKey], 'add');
+          updateMyWords(user.my_words[wordKey], "add");
         });
       }
     };
@@ -54,24 +52,16 @@ function App(props) {
   console.log(request.isReadyToGetVideos);
 
   if (request.isReadyToGetVideos) {
-    console.log('READEY TO GET VIDEOS');
+    console.log("READEY TO GET VIDEOS");
     const requestVideoData = async () => {
       const postResponse = await axios.post(
-        SERVER_URL +
-          `${REQUEST.POST_SEARCH_RESULT_FOR_VIDEO}/${selected.word}`,
+        SERVER_URL + `${REQUEST.POST_SEARCH_RESULT_FOR_VIDEO}/${selected.word}`,
         {
-          selected,
+          selected
         }
       );
-      // const postResponse = await axios.post(
-      //   SERVER_URL +
-      //     `${REQUEST.POST_SEARCH_RESULT_FOR_VIDEO}/query=${selected.word}&language=${selected.language}&categories=${selected.categories}`,
-      //   {
-      //     selected,
-      //   }
-      // );
-      console.log('fetchVideoData 실행');
-      console.log('fromServer:', postResponse.data.searched);
+      console.log("fetchVideoData 실행");
+      console.log("fromServer:", postResponse.data.searched);
       const foundWord = postResponse.data.searched.word;
       let info = postResponse.data.searched.videosInfo;
 
@@ -79,68 +69,25 @@ function App(props) {
         info.forEach(videoInfo => {
           videoInfo.captions.forEach(caption => {
             const startTime = Math.floor(Number(caption.start));
-            const time = secondsConverter(startTime, 'sec');
+            const time = secondsConverter(startTime, "sec");
             const mins = oneToTwoDigits(time.hours / 60 + time.minutes);
             const secs = oneToTwoDigits(time.seconds);
             caption.startForDisplay = `${mins}:${secs}`;
           });
         });
         getVideoData({ foundWord, info });
-        console.log('foundWord', foundWord);
+        console.log("foundWord", foundWord);
         if (selected.word === foundWord) {
-          console.log('addVideoInfoToDic');
+          console.log("addVideoInfoToDic");
           addVideoInfoToDictionary(info);
         }
       } else {
-        console.log('Cannot find any videos!');
-        getVideoData({ foundWord: '$NO_DATA', info: [] });
+        console.log("Cannot find any videos!");
+        getVideoData({ foundWord: "$NO_DATA", info: [] });
       }
-      console.log('가져온 비디오 정보', info);
+      console.log("가져온 비디오 정보", info);
     };
     requestVideoData();
-
-    //requestVideoData
-    // const fetchVideoData = async () => {
-    //   console.log('fetchVideoData 실행');
-    //   const getResponse = await axios.get(
-    //     SERVER_URL + REQUEST.GET_VIDEO_SUCCESS,
-    //     {
-    //       headers: {
-    //         Accept: 'application/json',
-    //         'Content-Type': 'application/json',
-    //         'Access-Control-Allow-Credentials': true,
-    //       },
-    //     }
-    //   );
-    //   console.log('fromServer:', getResponse.data.searched);
-    //   const foundWord = getResponse.data.searched.word;
-    //   let info = getResponse.data.searched.videosInfo;
-
-    //   if (info.length !== 0) {
-    //     info.forEach(videoInfo => {
-    //       videoInfo.captions.forEach(caption => {
-    //         const startTime = Math.floor(Number(caption.start));
-    //         const time = secondsConverter(startTime, 'sec');
-    //         const mins = oneToTwoDigits(time.hours / 60 + time.minutes);
-    //         const secs = oneToTwoDigits(time.seconds);
-    //         caption.startForDisplay = `${mins}:${secs}`;
-    //       });
-    //     });
-    //     getVideoData({ foundWord, info });
-    //     console.log('foundWord', foundWord);
-    //     if (selected.word === foundWord) {
-    //       console.log('addVideoInfoToDic');
-    //       addVideoInfoToDictionary(info);
-    //     }
-    //   } else {
-    //     console.log('Cannot find any videos!');
-    //     getVideoData({ foundWord: '$NO_DATA', info: [] });
-    //   }
-    //   console.log('가져온 비디오 정보', info);
-    // };
-    // fetchVideoData();
-
-    //requestDictionaryData
 
     const requestDictionaryData = async () => {
       //check vaild word - input 에서 미리 확인하기
@@ -148,37 +95,37 @@ function App(props) {
         const postDictionaryResponse = await axios.post(
           SERVER_URL + REQUEST.POST_SEARCH_RESULT_FOR_DICTIONARY,
           {
-            word: selected.word,
+            word: selected.word
           }
         );
-        console.log('사전 결과:', postDictionaryResponse.data.dictionary);
+        console.log("사전 결과:", postDictionaryResponse.data.dictionary);
         let dictionary = postDictionaryResponse.data.dictionary;
         getDictionaryData(dictionary);
       } else {
-        console.log('selected word is not valid');
+        console.log("selected word is not valid");
       }
     };
     requestDictionaryData();
   }
 
   if (request.isReadyToPostWord) {
-    console.log('post 요청 myWords:', myWords);
-    console.log('요청 보낼 데이터:', userInfo.google_id, myWords);
+    console.log("post 요청 myWords:", myWords);
+    console.log("요청 보낼 데이터:", userInfo.google_id, myWords);
     const requestPostAddedWord = async () => {
       const postMyWordResponse = await axios.post(
         SERVER_URL + REQUEST.POST_ADDED_WORD,
         {
           google_id: userInfo.google_id,
-          myWords,
+          myWords
         }
       );
-      console.log('postMyWordResponse:', postMyWordResponse);
+      console.log("postMyWordResponse:", postMyWordResponse);
     };
     requestPostAddedWord();
   }
 
   if (request.isReadyToDeleteWord.value) {
-    console.log('삭제 요청 보낼 데이터:', userInfo.google_id);
+    console.log("삭제 요청 보낼 데이터:", userInfo.google_id);
     let word = request.isReadyToDeleteWord.target;
     console.log(word);
     const requestDeleteWord = async () => {
@@ -187,42 +134,39 @@ function App(props) {
         {
           data: {
             google_id: userInfo.google_id,
-            word,
-          },
+            word
+          }
         }
       );
-      console.log('deleteMyWordResponse:', deleteMyWordResponse);
+      console.log("deleteMyWordResponse:", deleteMyWordResponse);
     };
     requestDeleteWord();
   }
 
   return (
-    <div className="App">
-      {window.location.pathname !== '/login' && (
-        <header className="App-header">
+    <div className='App'>
+      {window.location.pathname !== "/login" && (
+        <header className='App-header'>
           <Header {...props} />
         </header>
       )}
 
       <Route
         exact
-        path="/"
+        path='/'
         render={renderProps => <Home {...props} {...renderProps} />}
       />
-      <Route exact path="/login" render={() => <Login />} />
+      <Route exact path='/login' render={() => <Login />} />
       <Route
-        path="/videos"
+        path='/videos'
         render={renderProps => <Videos {...props} {...renderProps} />}
       />
       <Route
-        path="/myWords"
+        path='/myWords'
         render={renderProps => <MyWords {...props} {...renderProps} />}
       />
-      {/* <Route
-        path="/error"
-        render={renderProps => <Error {...props} {...renderProps} />}
-      /> */}
-      {/* <Route
+      {/* will be updated
+        <Route
         path="/myChannels"
         render={renderProps => (
           <MyChannel {...props} {...renderProps} />
